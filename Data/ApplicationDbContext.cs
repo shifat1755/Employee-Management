eaff28176e16d832fb7889984bfa35c4ab35cfa3
Syed_Shifat <shifat1755@gmail.com>
2024-09-13 01:40:49 +0600

    db ready

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeeManagement.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeManagement.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        
+        [Phone]
+        public string Phone { get; set; }
+        public DateOnly DOB { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? ProfileImg { get; set; }
+    }
+}
